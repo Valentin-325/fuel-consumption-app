c0147fc0e7c63c5c48b8894597546f19b37f217c
Valentin-325 <75860788+Valentin-325@users.noreply.github.com>
2022-08-25 01:03:02 +0300

    deployed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [fuel, setFuel] = useState('')
+  const [km, setKm] = useState('')
+  const [calc, setCalc] = useState('')
+  const [msg, setMsg] = useState('')
+
+
+  const calcConsumption = (e) => {
+    e.preventDefault()
+
+    if(fuel === 0 && km === 0) {
+      alert('Completeaza cele 2 campuri!!')
+    } else {
+      let calc = (fuel * 100 / km)
+      setCalc(calc.toFixed(1))
+    }
+
+    setFuel('')
+    setKm('')
+    setMsg('l/100km') 
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="container">
+          <div className="titleForm">
+            <h1>Fuel Consumption</h1>
+          </div>
+
+          <form onSubmit={calcConsumption}>
+            <div className="inputFuel">
+              <label htmlFor="fuelAdded">Fuel added:</label> <br />
+              <input type="text" placeholder='0' name="combustibil" id="combustibil" value={fuel} onChange={(e) => setFuel(e.target.value)}/>
+            </div>
+            <div className="inputKm">
+              <label htmlFor="kilometersTraveled">kilometers traveled:</label> <br />
+              <input type="text" placeholder='0' name="kilometri" id="km" value={km} onChange={(e) => setKm(e.target.value)}/>
+            </div>
+            <button className='btn' type='submit'>Calculate</button>
+          </form>
+        
+        <div className="displayConsumption">
+          <p>Your Consumption is: {calc} {msg}</p>
+        </div>
+      </div>
+
     </div>
   );
 }
